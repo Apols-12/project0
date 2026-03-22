306eb1fd579643ad79153be8d35d3e0500cda2a8
Apols-12 <ngassiaapollinaire4@gmail.com>
2026-03-22 11:19:00 +0000

    solve memory leak problem 3

--- src/main/kotlin/model/BackgrounWork.kt
+++ src/main/kotlin/model/BackgrounWork.kt
@@ -45,9 +45,7 @@ class BackgrounWork (private val baseUrl: String) {
         scope.launch {
             while (isActive) {
                 try {
-                   val response = client.use {
-                       it.get("$baseUrl/health")
-                   }
+                   val response = client.get("$baseUrl/health")
                     logger.info("the server is up: ${response.status}")
                     delay(10.minutes)
                 } catch (e: Exception) {
